4e1a04f86fbfc91d3407c877bd691c89143f5823
riyajath-ahamed <riyajatha@gmail.com>
2024-03-11 23:49:16 +0530

    complete

--- api/server.go
+++ api/server.go
@@ -24,7 +24,15 @@ func NewServer() *Server {
 		Router:        mux.NewRouter(),
 		shoppingItems: []Item{},
 	}
+	s.routes()
 	return s
+}
+
+func (s *Server) routes() {
+	s.HandleFunc("/shopping-items", s.createShoppingItem()).Methods("POST")
+	s.HandleFunc("/shopping-items", s.listShoppingItems()).Methods("GET")
+	s.HandleFunc("/shopping-items/{id}", s.removeShoppingItem()).Methods("DELETE")
+
 }
 
 func (s *Server) createShoppingItem() http.HandlerFunc {
@@ -39,6 +47,38 @@ func (s *Server) createShoppingItem() http.HandlerFunc {
 		s.shoppingItems = append(s.shoppingItems, i)
 
 		w.Header().Set("Content-Type", "application/json")
-		//if err := json.NewDecoder(w).E
+		if err := json.NewEncoder(w).Encode(i); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func (s *Server) listShoppingItems() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		if err := json.NewEncoder(w).Encode(s.shoppingItems); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func (s *Server) removeShoppingItem() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		idStr := mux.Vars(r)["id"]
+		id, err := uuid.Parse(idStr)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		for i, item := range s.shoppingItems {
+			if item.ID == id {
+				s.shoppingItems = append(s.shoppingItems[:i], s.shoppingItems[i+1:]...)
+				break
+			}
+		}
 	}
 }
--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
+import (
+	"example/main/api"
+	"net/http"
+)
+
 func main() {
 	// Code
+	srv := api.NewServer()
+	http.ListenAndServe(":8080", srv)
 
 }
